f470fa226140b6a12047dc26556f12a44fd33ef3
Raluca <dodisiraluca19@stud.ase.ro>
2024-05-05 19:29:53 +0300

    added CRUD methods

--- my-app/pages/api/hello.js
+++ my-app/pages/api/hello.js
@@ -1,5 +1,11 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+import { sendMethodNotAllowed } from "@/utils/apiMethods";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+  const isAllowedMethod = req.method === 'GET' || req.method === 'PUT' ||req.method === 'DELETE';
+
+  if (!isAllowedMethod){
+    return sendMethodNotAllowed(res, 'Method Not Allowed');
+  }
 }
